21eaba46ed9cac81ab16159c6d4a2cfe1bf7720c
oliver2ba <oliver.belliardabreu@gmail.com>
2024-12-22 13:33:46 +0100

    SPI connection fixed: data size set to 8 bits

--- Projet_V-NOM_STM32/Core/Src/main.c
+++ Projet_V-NOM_STM32/Core/Src/main.c
@@ -45,6 +45,13 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define DEBUG 1 // Enable debug logs
+
+#if DEBUG
+#define DEBUG_PRINT(...) printf(__VA_ARGS__)
+#else
+#define DEBUG_PRINT(...)
+#endif
 
 /* USER CODE END PD */
 
@@ -202,10 +209,10 @@ void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart)
 void test_YLIDARX2(void)
 {
 	printf("YLIDAR X2 Initialization...\r\n");
-		hYLIDAR.uart_buffer = &rxByte;
-		// Start UART reception in interrupt mode (1 byte at a time)
-		HAL_UART_Receive_IT(&huart2, &rxByte, 1);
-		printf("YLIDAR X2 Initialization Successful!\r\n");
+	hYLIDAR.uart_buffer = &rxByte;
+	// Start UART reception in interrupt mode (1 byte at a time)
+	HAL_UART_Receive_IT(&huart2, &rxByte, 1);
+	printf("YLIDAR X2 Initialization Successful!\r\n");
 }
 
 void test_Motors(void)
@@ -213,9 +220,9 @@ void test_Motors(void)
 	Motor_Init(&hMotors, &htim1);
 
 	hMotors.mode_mot1 = FORWARD_MODE;
-		hMotors.mode_mot2 = FORWARD_MODE;
-		Motor_SetMode(&hMotors);
-		Motor_SetSpeed_percent(&hMotors, 90.0, 90.0);
+	hMotors.mode_mot2 = FORWARD_MODE;
+	Motor_SetMode(&hMotors);
+	Motor_SetSpeed_percent(&hMotors, 90.0, 90.0);
 }
 
 void test_ADXL(void)
@@ -267,7 +274,10 @@ void test_ADXL(void)
 	ADXL_Standby(ON);
 }
 
+//////////////////////////////////////////////////
 //////////////////// ADXL343 ////////////////////
+////////////////////////////////////////////////
+
 /*
  * https://controllerstech.com/adxl345-accelerometer-using-stm32/
  */
@@ -282,32 +292,32 @@ void test_ADXL(void)
  */
 void spiWrite(uint8_t address, uint8_t *data, uint16_t length)
 {
-    HAL_StatusTypeDef status;
+	HAL_StatusTypeDef status;
 
-    // Set the write command (clear MSB of the register address)
-    uint8_t writeAddress = address & 0x7F;
+	// Set the write command (clear MSB of the register address)
+	uint8_t writeAddress = address & 0x7F;
 
-    // Pull CS low to start the SPI transaction
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_RESET);
+	// Pull CS low to start the SPI transaction
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_RESET);
 
-    // Transmit the address
-    status = HAL_SPI_Transmit(&hspi1, &writeAddress, 1, HAL_MAX_DELAY);
-    if (status != HAL_OK)
-    {
-        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_SET); // Set CS high on error
-        printf("SPI Write Error during address transmission!\n");
-        Error_Handler();
-    }
+	// Transmit the address
+	status = HAL_SPI_Transmit(&hspi1, &writeAddress, 1, HAL_MAX_DELAY);
+	if (status != HAL_OK)
+	{
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_SET); // Set CS high on error
+		printf("SPI Write Error during address transmission!\n");
+		Error_Handler();
+	}
 
-    // Transmit the data
-    status = HAL_SPI_Transmit(&hspi1, data, length, HAL_MAX_DELAY);
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_SET); // Set CS high after transaction
+	// Transmit the data
+	status = HAL_SPI_Transmit(&hspi1, data, length, HAL_MAX_DELAY);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_SET); // Set CS high after transaction
 
-    if (status != HAL_OK)
-    {
-        printf("SPI Write Error during data transmission!\n");
-        Error_Handler();
-    }
+	if (status != HAL_OK)
+	{
+		printf("SPI Write Error during data transmission!\n");
+		Error_Handler();
+	}
 }
 
 
@@ -319,32 +329,31 @@ void spiWrite(uint8_t address, uint8_t *data, uint16_t length)
  */
 void spiRead(uint8_t address, uint8_t *data, uint16_t length)
 {
-    HAL_StatusTypeDef status;
+	HAL_StatusTypeDef status;
 
-    // Set the read command (set MSB of the register address)
-    uint8_t readAddress = address | 0x80;
+	// Pull CS low to start the SPI transaction
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_RESET);
 
-    // Pull CS low to start the SPI transaction
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_RESET);
+	// Transmit the address
+	status = HAL_SPI_Transmit(&hspi1, &address, 1, HAL_MAX_DELAY);
+	DEBUG_PRINT("Transmission status: 0x%X\r\n", status);
+	if (status != HAL_OK)
+	{
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_SET); // Set CS high on error
+		printf("SPI Read Error during address transmission!\n");
+		Error_Handler();
+	}
 
-    // Transmit the address
-    status = HAL_SPI_Transmit(&hspi1, &readAddress, 1, HAL_MAX_DELAY);
-    if (status != HAL_OK)
-    {
-        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_SET); // Set CS high on error
-        printf("SPI Read Error during address transmission!\n");
-        Error_Handler();
-    }
+	// Receive the data
+	status = HAL_SPI_Receive(&hspi1, data, length, HAL_MAX_DELAY);
+	DEBUG_PRINT("Reception status: 0x%X\r\n", status);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_SET); // Set CS high after transaction
 
-    // Receive the data
-    status = HAL_SPI_Receive(&hspi1, data, length, HAL_MAX_DELAY);
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_SET); // Set CS high after transaction
-
-    if (status != HAL_OK)
-    {
-        printf("SPI Read Error during data reception!\n");
-        Error_Handler();
-    }
+	if (status != HAL_OK)
+	{
+		printf("SPI Read Error during data reception!\n");
+		Error_Handler();
+	}
 }
 
 
@@ -354,104 +363,117 @@ void spiRead(uint8_t address, uint8_t *data, uint16_t length)
  * @param reg The register address to write to.
  * @param value The value to write to the register.
  */
-void ADXL343_writeRegister(uint8_t reg, uint8_t value) {
-    // Set the write command by clearing the MSB of the register address
-    uint8_t writeAddress = reg & 0x7F;
-    spiWrite(writeAddress, value);
+void ADXL343_writeRegister(uint8_t reg, uint8_t * values, uint16_t length) {
+	// Setting R/W = 0, i.e.: Write Mode
+	uint8_t writeAddress = reg & ~(0x80);
+
+	spiWrite(writeAddress, values, length);
 }
 
 /**
  * @brief Reads a value from a specified register on the ADXL343.
  *
  * @param reg The register address to read from.
- * @return uint8_t The value read from the register.
+ * @num		: number of bytes to be read.
  */
-uint8_t ADXL343_readRegister(uint8_t reg) {
-    // Set the read command by setting the MSB of the register address
-    //uint8_t readAddress = reg | 0x80;
-	uint8_t readAddress = reg & ~(0x80);
-    return spiRead(readAddress);
+void ADXL343_readRegister(uint8_t reg, uint8_t *data, uint16_t num) {
+	if (reg > 63)
+		reg = 63;
+
+	// Multiple Byte Read Settings
+	if (num > 1)
+		reg |= 0x40;
+	else
+		reg &= ~(0x40);
+
+	// Setting R/W = 1, i.e.: Read Mode
+	reg |= (0x80);
+
+	DEBUG_PRINT("Reading register 0x%X\r\n", reg);
+	spiRead(reg, data, num);
 }
 
 /**
  * @brief Tests communication by reading the Device ID register.
  */
 void testDeviceID(void) {
+	DEBUG_PRINT("Setting CSn\r\n");
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_SET); // Set CS high
 
-    uint8_t deviceID = ADXL343_readRegister(ADXL343_DEVICE_ID_REG);
+	uint8_t deviceID;
+	ADXL343_readRegister(ADXL343_DEVICE_ID_REG, &deviceID, 1);
 
-    if (deviceID == 0xE5) { // Device ID for ADXL343
-        printf("ADXL343 detected successfully! Device ID: 0x%02X\n", deviceID);
-    } else {
-        printf("Failed to detect ADXL343. Read Device ID: 0x%02X\n", deviceID);
-    }
+	if (deviceID == 0xE5) { // Device ID for ADXL343
+		printf("ADXL343 detected successfully! Device ID: 0x%02X\n", deviceID);
+	} else {
+		printf("Failed to detect ADXL343. Read Device ID: 0x%02X\n", deviceID);
+	}
 }
 
 /* USER CODE END 0 */
 
 /**
- * @brief  The application entry point.
- * @retval int
- */
+  * @brief  The application entry point.
+  * @retval int
+  */
 int main(void)
 {
 
-	/* USER CODE BEGIN 1 */
+  /* USER CODE BEGIN 1 */
 
-	/* USER CODE END 1 */
+  /* USER CODE END 1 */
 
-	/* MCU Configuration--------------------------------------------------------*/
+  /* MCU Configuration--------------------------------------------------------*/
 
-	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-	HAL_Init();
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-	/* USER CODE BEGIN Init */
+  /* USER CODE BEGIN Init */
 
-	/* USER CODE END Init */
+  /* USER CODE END Init */
 
-	/* Configure the system clock */
-	SystemClock_Config();
+  /* Configure the system clock */
+  SystemClock_Config();
 
-	/* USER CODE BEGIN SysInit */
+  /* USER CODE BEGIN SysInit */
 
-	/* USER CODE END SysInit */
+  /* USER CODE END SysInit */
 
-	/* Initialize all configured peripherals */
-	MX_GPIO_Init();
-	MX_SPI1_Init();
-	MX_TIM1_Init();
-	MX_TIM3_Init();
-	MX_TIM4_Init();
-	MX_USART2_UART_Init();
-	MX_USART1_UART_Init();
-	MX_ADC1_Init();
-	/* USER CODE BEGIN 2 */
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_SPI1_Init();
+  MX_TIM1_Init();
+  MX_TIM3_Init();
+  MX_TIM4_Init();
+  MX_USART2_UART_Init();
+  MX_USART1_UART_Init();
+  MX_ADC1_Init();
+  /* USER CODE BEGIN 2 */
 	printf("\r\n*** Waking up V-NOM ***\r\n");
 	printf("%s", jumbo_logo_msg);
 
 	/* Motors test & initialization *
 	test_Motors();
-	*/
+	 */
 
 	/* YLIDAR X2 test & Initialization *
 	test_YLIDARX2();
-	*/
+	 */
 
 	/* ToF sensors Initialization *
 	printf("GP2Y0A41SK0F Initialization...\r\n");
 	GP2Y0A41SK0F_Init(&hTof);
 	printf("GP2Y0A41SK0F Initialization Successful!\r\n");
-	*/
+	 */
 
 	/* ADXL343 Initialization */
 	//test_ADXL();
 	testDeviceID();
 
-	/* USER CODE END 2 */
+  /* USER CODE END 2 */
 
-	/* Infinite loop */
-	/* USER CODE BEGIN WHILE */
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
 	while (1)
 	{
 		/* ToF test *
@@ -461,59 +483,59 @@ int main(void)
 		Motor_SetSpeed_percent(&hMotors, hTof.distance_tof1, hTof.distance_tof1);
 		//printf("Mot1 speed: %d, Mot2 speed: %d\r\n", hMotors.current_speed1, hMotors.current_speed2);
 		Motor_UpdateSpeed(&hMotors);
-		*/
+		 */
 
-		/* USER CODE END WHILE */
+    /* USER CODE END WHILE */
 
-		/* USER CODE BEGIN 3 */
+    /* USER CODE BEGIN 3 */
 	}
-	/* USER CODE END 3 */
+  /* USER CODE END 3 */
 }
 
 /**
- * @brief System Clock Configuration
- * @retval None
- */
+  * @brief System Clock Configuration
+  * @retval None
+  */
 void SystemClock_Config(void)
 {
-	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-	/** Configure the main internal regulator output voltage
-	 */
-	HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1_BOOST);
+  /** Configure the main internal regulator output voltage
+  */
+  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1_BOOST);
 
-	/** Initializes the RCC Oscillators according to the specified parameters
-	 * in the RCC_OscInitTypeDef structure.
-	 */
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-	RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV4;
-	RCC_OscInitStruct.PLL.PLLN = 85;
-	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-	RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
-	RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
-	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-	{
-		Error_Handler();
-	}
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV4;
+  RCC_OscInitStruct.PLL.PLLN = 85;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-	/** Initializes the CPU, AHB and APB buses clocks
-	 */
-	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-			|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
-	{
-		Error_Handler();
-	}
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
 /* USER CODE BEGIN 4 */
@@ -521,33 +543,33 @@ void SystemClock_Config(void)
 /* USER CODE END 4 */
 
 /**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
 void Error_Handler(void)
 {
-	/* USER CODE BEGIN Error_Handler_Debug */
+  /* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	__disable_irq();
 	while (1)
 	{
 	}
-	/* USER CODE END Error_Handler_Debug */
+  /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
 /**
- * @brief  Reports the name of the source file and the source line number
- *         where the assert_param error has occurred.
- * @param  file: pointer to the source file name
- * @param  line: assert_param error line source number
- * @retval None
- */
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-	/* USER CODE BEGIN 6 */
+  /* USER CODE BEGIN 6 */
 	/* User can add his own implementation to report the file name and line number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-	/* USER CODE END 6 */
+  /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
