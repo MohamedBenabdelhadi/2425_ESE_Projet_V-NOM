2cd32032824d55697fd408ffa86e66f6ad6dcdec
oliver2ba <oliver.belliardabreu@gmail.com>
2025-01-10 09:10:14 +0100

    Doxygen comments added

--- Projet_V-NOM_STM32/Core/Src/main.c
+++ Projet_V-NOM_STM32/Core/Src/main.c
@@ -57,6 +57,8 @@
 #define STACK_SIZE 256
 #define TASK_PRIORITY_MOTOR 1
 
+#define TOF_TRESHHOLD 40
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -179,9 +181,16 @@ int __io_putchar(int ch)
 }
 
 /**
- * @brief UART receive half complete callback.
- * @param huart: Pointer to the UART handle
+ * @brief Interrupt callbacks.
+ * @details Contains interrupt service routines (ISRs) and related callbacks.
+ */
+
+/**
+ * @brief UART receive half-complete callback.
+ * @param huart: Pointer to the UART handle.
  * @retval None
+ * @details This callback processes partial DMA reception for the UART instance
+ * (e.g., used for YLIDAR data).
  */
 void HAL_UART_RxHalfCpltCallback(UART_HandleTypeDef *huart) {
 	if (huart->Instance == USART2) {
@@ -191,8 +200,9 @@ void HAL_UART_RxHalfCpltCallback(UART_HandleTypeDef *huart) {
 
 /**
  * @brief UART receive complete callback.
- * @param huart: Pointer to the UART handle
+ * @param huart: Pointer to the UART handle.
  * @retval None
+ * @details This callback processes complete DMA reception for the UART instance.
  */
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
 	if (huart->Instance == USART2) {
@@ -201,31 +211,11 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
 }
 
 /**
- * @brief UART error callback.
- * @param huart: Pointer to the UART handle
- * @retval None
- */
-void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart)
-{
-	if (huart->Instance == USART2)
-	{
-		if (HAL_UART_GetError(huart) & HAL_UART_ERROR_PE)
-			DEBUG_PRINT("Parity Error!\r\n");
-		if (HAL_UART_GetError(huart) & HAL_UART_ERROR_NE)
-			DEBUG_PRINT("Noise Error!\r\n");
-		if (HAL_UART_GetError(huart) & HAL_UART_ERROR_FE)
-			DEBUG_PRINT("Framing Error!\r\n");
-		if (HAL_UART_GetError(huart) & HAL_UART_ERROR_ORE)
-			DEBUG_PRINT("Overrun Error!\r\n");
-
-		//YLIDARX2_RestartDMA(&hlidar);
-	}
-}
-
-/**
  * @brief ADC conversion complete callback.
- * This function is called when an ADC conversion is complete.
- * It retrieves the ADC value and processes the data.
+ * @param hadc: Pointer to the ADC handle.
+ * @retval None
+ * @details This function processes ADC conversion data when the conversion
+ * is complete. It differentiates between ADC1 and ADC2 to process ToF sensor readings.
  */
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc) {
 	if (hadc->Instance == ADC1) {
@@ -238,67 +228,47 @@ void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc) {
 	}
 }
 
-void HAL_ADC_ErrorCallback(ADC_HandleTypeDef *hadc) {
-	if (hadc->ErrorCode != HAL_ADC_ERROR_NONE) {
-		printf("ADC Error Code: %lx\n", hadc->ErrorCode);
-
-		GP2Y0A41SK0F_Start_Interrupt(&hTof);
-	}
-}
-
 /**
  * @brief GPIO interrupt callback for EXTI line.
- * This function is triggered by the accelerometer interrupt.
+ * @param GPIO_Pin: The GPIO pin that triggered the interrupt.
+ * @retval None
+ * @details This callback handles the interrupt triggered by the accelerometer
+ * (single tap or other configured events).
  */
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
-    if (GPIO_Pin == AG_INT1_Pin) // Interrupt triggered by INT1
-    {
-        uint8_t int_source;
-        ADXL343_readRegister(ADXL343_INT_SOURCE, &int_source, 1);
+	if (GPIO_Pin == AG_INT1_Pin) // Interrupt triggered by INT1
+	{
+		uint8_t int_source;
+		ADXL343_readRegister(ADXL343_INT_SOURCE, &int_source, 1);
 
-        if (int_source & 0x40) // Check SINGLE_TAP bit
-        {
-        	DEBUG_PRINT("Single Tap Detected!\n");
-        	Motor_Stop(&hMotors);
-        	vTaskDelete(xHandle2);
-        }
-    }
-    else if (GPIO_Pin == AG_INT2_Pin) // Interrupt triggered by INT2
-    {
-    	DEBUG_PRINT("Interrupt triggered on INT2\n");
-        // Handle other potential events from INT2 if needed
-    }
-}
-
-/**
- * @brief  Gestion d'erreurs de création de tâche.
- * @param	BaseType_t	Retour de xTaskCreate.
- */
-void errHandler_xTaskCreate(BaseType_t r)
-{
-	/* Vérification si la tâche a été créée avec succès */
-	if (pdPASS == r) {
-		/* Si la tâche est créée avec succès, démarrer le scheduler */
-		DEBUG_PRINT("Tâche crée avec succès\r\n");
-	} else if (errCOULD_NOT_ALLOCATE_REQUIRED_MEMORY == r) {
-		DEBUG_PRINT("Erreur: Mémoire insuffisante\r\n");
-		Error_Handler();
-	} else {
-		/* Cas improbable : code d'erreur non prévu pour xTaskCreate */
-		DEBUG_PRINT("Erreur inconnue lors de la création de la tâche\r\n");
-		Error_Handler();  	// Gestion d'erreur générique
-		NVIC_SystemReset(); // Réinitialiser le microcontrôleur
+		if (int_source & 0x40) // Check SINGLE_TAP bit
+		{
+			DEBUG_PRINT("Single Tap Detected!\n");
+			Motor_Stop(&hMotors);
+			vTaskDelete(xHandle2);
+		}
+	}
+	else if (GPIO_Pin == AG_INT2_Pin) // Interrupt triggered by INT2
+	{
+		DEBUG_PRINT("Interrupt triggered on INT2\n");
+		// Handle other potential events from INT2 if needed
 	}
 }
 
-#define TOF_TRESHHOLD 40
+/**
+ * @brief Task declarations.
+ * @details Contains declarations for FreeRTOS tasks in the system.
+ */
 
+/**
+ * @brief Motor control task.
+ * @param unsused: Unused parameter.
+ * @retval None
+ * @details This FreeRTOS task continuously updates the motor speed based on current settings.
+ */
 void task_Motors(void * unsused)
 {
-	/* Motors test */
-	Motor_Init(&hMotors, &htim1);
-
 	while (1)
 	{
 		DEBUG_PRINT("Mot1 speed: %d, Mot2 speed: %d\r\n", hMotors.current_speed1, hMotors.current_speed2);
@@ -306,6 +276,13 @@ void task_Motors(void * unsused)
 	}
 }
 
+/**
+ * @brief Behavior control task.
+ * @param unsused: Unused parameter.
+ * @retval None
+ * @details This FreeRTOS task controls motor behavior based on ToF sensor readings.
+ * It evaluates distances from ToF sensors and adjusts motor directions accordingly.
+ */
 void task_Behaviour(void * unsused)
 {
 	while (1)
@@ -345,52 +322,89 @@ void task_Behaviour(void * unsused)
 	}
 }
 
+/**
+ * @brief Handles errors during task creation.
+ * @param r: Return value from xTaskCreate.
+ * @retval None
+ * @details This function checks the return value of `xTaskCreate` to determine
+ * if a task was created successfully. In case of failure, it performs error handling.
+ */
+void errHandler_xTaskCreate(BaseType_t r)
+{
+	/* Vérification si la tâche a été créée avec succès */
+	if (pdPASS == r) {
+		/* Si la tâche est créée avec succès, démarrer le scheduler */
+		DEBUG_PRINT("Tâche crée avec succès\r\n");
+	} else if (errCOULD_NOT_ALLOCATE_REQUIRED_MEMORY == r) {
+		DEBUG_PRINT("Erreur: Mémoire insuffisante\r\n");
+		Error_Handler();
+	} else {
+		/* Cas improbable : code d'erreur non prévu pour xTaskCreate */
+		DEBUG_PRINT("Erreur inconnue lors de la création de la tâche\r\n");
+		Error_Handler();  	// Gestion d'erreur générique
+		NVIC_SystemReset(); // Réinitialiser le microcontrôleur
+	}
+}
+
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
 
-  /* USER CODE BEGIN 1 */
+	/* USER CODE BEGIN 1 */
 	BaseType_t xReturned;
 
-  /* USER CODE END 1 */
+	/* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+	/* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+	/* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+	/* USER CODE BEGIN SysInit */
+	/**
+	 * @brief Peripheral initialization.
+	 * @details Contains initialization code for GPIO, ADC, SPI, TIM, etc.
+	 */
+	/* USER CODE END SysInit */
 
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_SPI1_Init();
-  MX_TIM1_Init();
-  MX_TIM3_Init();
-  MX_TIM4_Init();
-  MX_USART2_UART_Init();
-  MX_USART1_UART_Init();
-  MX_ADC1_Init();
-  MX_ADC2_Init();
-  /* USER CODE BEGIN 2 */
-	printf("\r\n*** Waking up V-NOM ***\r\n");
-	//printf("%s", jumbo_logo_msg);
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_SPI1_Init();
+	MX_TIM1_Init();
+	MX_TIM3_Init();
+	MX_TIM4_Init();
+	MX_USART2_UART_Init();
+	MX_USART1_UART_Init();
+	MX_ADC1_Init();
+	MX_ADC2_Init();
+	/* USER CODE BEGIN 2 */
+	DEBUG_PRINT("\r\n*** Waking up V-NOM ***\r\n");
+	//DEBUG_PRINT("%s", jumbo_logo_msg);
 
 	/* ToF sensors Initialization */
 	GP2Y0A41SK0F_Init(&hTof);
+
+	/* ADXL343 Initialization */
+	ADXL343_Init(&hADXL);
+
+	/* Motors Initialization */
+	Motor_Init(&hMotors, &htim1);
+
+	/* YLIDAR X2 Initialization with DMA *
+	LIDAR_RX_GPIO_Port->PUPDR = GPIO_PULLUP;
+	YLIDARX2_InitDMA(&hlidar, &huart2);*/
 
 	/* Motors task */
 	xReturned = xTaskCreate(
@@ -414,136 +428,161 @@ int main(void)
 
 	errHandler_xTaskCreate(xReturned);
 
-	/* YLIDAR X2 Initialization with DMA *
-	LIDAR_RX_GPIO_Port->PUPDR = GPIO_PULLUP;
-	YLIDARX2_InitDMA(&hlidar, &huart2);*/
+	/* USER CODE END 2 */
 
-	/* ADXL343 Initialization *
-	ADXL343_Init(&hADXL);*/
+	/* Call init function for freertos objects (in cmsis_os2.c) */
+	MX_FREERTOS_Init();
 
-  /* USER CODE END 2 */
+	/* Start scheduler */
+	osKernelStart();
 
-  /* Call init function for freertos objects (in cmsis_os2.c) */
-  MX_FREERTOS_Init();
+	/* We should never get here as control is now taken by the scheduler */
 
-  /* Start scheduler */
-  osKernelStart();
-
-  /* We should never get here as control is now taken by the scheduler */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
 	while (1)
 	{
 		/* ADXL343 test *
 		ADXL343_get_Acceleration(&hADXL);*/
 
-    /* USER CODE END WHILE */
+		/* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
+		/* USER CODE BEGIN 3 */
 	}
-  /* USER CODE END 3 */
+	/* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-  */
-  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1_BOOST);
+	/** Configure the main internal regulator output voltage
+	 */
+	HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1_BOOST);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV4;
-  RCC_OscInitStruct.PLL.PLLN = 85;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
-  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+	RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV4;
+	RCC_OscInitStruct.PLL.PLLN = 85;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+	RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	{
+		Error_Handler();
+	}
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+			|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+	{
+		Error_Handler();
+	}
 }
 
 /* USER CODE BEGIN 4 */
 
-/* USER CODE END 4 */
+void HAL_ADC_ErrorCallback(ADC_HandleTypeDef *hadc) {
+	if (hadc->ErrorCode != HAL_ADC_ERROR_NONE) {
+		printf("ADC Error Code: %lx\n", hadc->ErrorCode);
 
-/**
-  * @brief  Period elapsed callback in non blocking mode
-  * @note   This function is called  when TIM2 interrupt took place, inside
-  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
-  * a global variable "uwTick" used as application time base.
-  * @param  htim : TIM handle
-  * @retval None
-  */
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
-{
-  /* USER CODE BEGIN Callback 0 */
-
-  /* USER CODE END Callback 0 */
-  if (htim->Instance == TIM2) {
-    HAL_IncTick();
-  }
-  /* USER CODE BEGIN Callback 1 */
-
-  /* USER CODE END Callback 1 */
+		GP2Y0A41SK0F_Start_Interrupt(&hTof);
+	}
 }
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief UART error callback.
+ * @param huart: Pointer to the UART handle.
+ * @retval None
+ * @details This function handles UART errors such as parity, noise, framing,
+ * or overrun errors. It restarts the LIDAR's DMA in case of errors.
+ */
+void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart)
+{
+	if (huart->Instance == USART2)
+	{
+		if (HAL_UART_GetError(huart) & HAL_UART_ERROR_PE)
+			DEBUG_PRINT("Parity Error!\r\n");
+		if (HAL_UART_GetError(huart) & HAL_UART_ERROR_NE)
+			DEBUG_PRINT("Noise Error!\r\n");
+		if (HAL_UART_GetError(huart) & HAL_UART_ERROR_FE)
+			DEBUG_PRINT("Framing Error!\r\n");
+		if (HAL_UART_GetError(huart) & HAL_UART_ERROR_ORE)
+			DEBUG_PRINT("Overrun Error!\r\n");
+
+		//YLIDARX2_RestartDMA(&hlidar);
+	}
+}
+
+/* USER CODE END 4 */
+
+/**
+ * @brief  Period elapsed callback in non blocking mode
+ * @note   This function is called  when TIM2 interrupt took place, inside
+ * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+ * a global variable "uwTick" used as application time base.
+ * @param  htim : TIM handle
+ * @retval None
+ */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+	/* USER CODE BEGIN Callback 0 */
+
+	/* USER CODE END Callback 0 */
+	if (htim->Instance == TIM2) {
+		HAL_IncTick();
+	}
+	/* USER CODE BEGIN Callback 1 */
+
+	/* USER CODE END Callback 1 */
+}
+
+/**
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
+	/* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	__disable_irq();
 	while (1)
 	{
 	}
-  /* USER CODE END Error_Handler_Debug */
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
+	/* USER CODE BEGIN 6 */
 	/* User can add his own implementation to report the file name and line number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+	/* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
